aa63f595e99e83d91734f84ede73de98c23dd95a
Emma <emmahlowe@gmail.com>
2019-12-04 19:21:13 -0700

    More dynamic baby

--- INTEXPROJECT/INTEXPROJECT/Controllers/OrderController.cs
+++ INTEXPROJECT/INTEXPROJECT/Controllers/OrderController.cs
@@ -15,7 +15,8 @@ namespace INTEXPROJECT.Controllers
             new AssayDescriptions{Assay_Description_ID = 2, Price_ID = 1, Type_ID = 1, Description = "DiscoveryScreen", Protocol = "Test Protocol", Completion_Time = 300, Literature_References="Something"},
             new AssayDescriptions{Assay_Description_ID = 3, Price_ID = 1, Type_ID = 1, Description = "ImmunoScreen", Protocol = "Test Protocol", Completion_Time = 300, Literature_References="Something"},
             new AssayDescriptions{Assay_Description_ID = 4, Price_ID = 1, Type_ID = 1, Description = "ProfilingScreen", Protocol = "Test Protocol", Completion_Time = 300, Literature_References="Something"},
-            new AssayDescriptions{Assay_Description_ID = 5, Price_ID = 1, Type_ID = 1, Description = "PharmaScreen", Protocol = "Test Protocol", Completion_Time = 300, Literature_References="Something"}
+            new AssayDescriptions{Assay_Description_ID = 5, Price_ID = 1, Type_ID = 1, Description = "PharmaScreen", Protocol = "Test Protocol", Completion_Time = 300, Literature_References="Something"},
+            new AssayDescriptions{Assay_Description_ID = 6, Price_ID = 1, Type_ID = 1, Description = "CustomScreen", Protocol = "Test Protocol", Completion_Time = 300, Literature_References="Something"}
         };
 
         public static List<CompoundDescriptions> lstCompounds = new List<CompoundDescriptions>()
@@ -66,28 +67,53 @@ namespace INTEXPROJECT.Controllers
             ViewBag.Priorities = lstPriorities;
             return View();
         }
-        public static int numAssays = 0;
-        public static int numCompounds;
+        public static int numCompounds = 0;
 
         public ActionResult AddAssay()
         {
-            numAssays += 1;
-            for (int iCount = 0; iCount < numAssays; iCount++)
+
+            ViewBag.Compounds = lstCompounds;
+            ViewBag.Assays = lstAssays;
+            ViewBag.Priorities = lstPriorities;
+            return View("Create");
+        }
+
+        public ActionResult AddCompound()
+        {
+            numCompounds += 1;
+            for (int iCount = 0; iCount < numCompounds; iCount++)
             {
-                ViewBag.addAssay += "<h5> Compound " + (iCount + 2) + " </h5> " +
+                ViewBag.addCompound += "<h5> Compound " + (iCount + 2) + " </h5> " +
             "<label for= 'compName" + (iCount + 2) + "'>Compound Name</label> " +
-            "<input type='text' name='compName" +  (iCount + 2) + "' /> " +
-             "<br />" +
+            "<select name='compName" + (iCount + 2) + " > " +
+                "<option value='0'>Serotonin</option> " +
+                "<option value='1'>Atropine</option> " +
+                "<option value='2'>Tyrosine</option> " +
+                "<option value='3'>Quinine</option> " +
+                 "<option value='4'>Phenobarbital</option> " +
+            "  </select> " +
+
+            "<br />" +
 
              "<label for= 'compWeight" + (iCount + 2) + "'>Compound Weight</label> " +
              "<input type='text' name='compWeight" + (iCount + 2) + "' /> " +
-             "<br />" +
+             "<br />";
 
-            "<label for= 'assayTest" + (iCount + 2) + "'>Assay Test</label> " +
-             "<input type='text' name='assayTest" + (iCount + 2) + "' /> ";
-             
+             for (int inCount = 1; inCount < 6; inCount++)
+                {
+                    ViewBag.addCompound += "<label for= 'assayTest" + (iCount + 2) + "." + inCount + "'>Assay Test</label> " +
+            "<select name='assayTest" + (iCount + 2) + "." + inCount + " > " +
+                "<option value='0'>No test</option> " +
+                "<option value='1'>Biochemical Pharmacology</option> " +
+                "<option value='2'>DiscoveryScreen</option> " +
+                "<option value='3'>ImmunoScreen</option> " +
+                "<option value='4'>ProfilingScreen</option> " +
+                "<option value='4'>PharmaScreen</option> " +
+                "<option value='4'>CustomScreen</option> " +
+            "  </select> <br />";
+                }
+
             }
-
             ViewBag.Compounds = lstCompounds;
             ViewBag.Assays = lstAssays;
             ViewBag.Priorities = lstPriorities;
